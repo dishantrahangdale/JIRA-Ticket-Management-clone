876e2634e484fd5b62f9ecf2bd73fa4e1a555273
dishantrahangdale <dishant.rahangdale@gmail.com>
2022-01-17 17:23:10 +0530

    appending error

--- index_script.js
+++ index_script.js
@@ -1,5 +1,6 @@
 let add_btn = document.querySelector(".add_btn");
 let modal_cont = document.querySelector(".modal_cont");
+let mainCont = document.querySelector("main_cont");
 let addflag = false;
 
 add_btn.addEventListener("click",(e)=>{
@@ -12,3 +13,27 @@ add_btn.addEventListener("click",(e)=>{
         modal_cont.style.display = "none";
     }
 })
+
+modal_cont.addEventListener("keydown", (e) => {
+    let key = e.key;
+    if(key==="Shift"){
+        createNewTicket();
+        // modal_cont.style.display = "none";
+        // modal_cont.innerText = "";
+        // console.log("hi dishant");
+    }
+})
+
+function createNewTicket(){
+    let ticketcont = document.createElement("div");
+    ticketcont.setAttribute("class","ticket_cont");
+    ticketcont.innerHTML = `
+        <div class="ticket_color"></div>
+        <div class="ticket_id">sample</div>
+        <div class="ticket_task">SFDFDD</div>
+    `;
+    // console.log(ticketcont);
+    mainCont.appendChild(ticketcont);
+    // ticketcont.async = false;
+    // document.getElementById("main_cont").appendChild(ticketcont);
+}
